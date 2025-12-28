d1210de91c82b5118e0b359a5457221139bbd90f
Dinanath <dinumishr725@gmail.com>
2025-12-29 02:19:06 +0530

    Fix mvnw permission for Docker build 3

--- src/main/java/com/portal/everyday/EveryDayHelpApplication.java
+++ src/main/java/com/portal/everyday/EveryDayHelpApplication.java
@@ -1,29 +1,16 @@
 package com.portal.everyday;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.portal.everyday.service.MailTest;
-
-
 
 @SpringBootApplication
-public class EveryDayHelpApplication implements CommandLineRunner {
+public class EveryDayHelpApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EveryDayHelpApplication.class, args);
 	
 	}
 
-@Autowired	
-private MailTest mailTest;	
-	@Override
-	public void run(String... args) throws Exception {
-		// TODO Auto-generated method stub
-		//mailTest.sendTestMail();
-		
-	}
 
 }
